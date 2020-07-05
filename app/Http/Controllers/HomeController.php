7f5d432f044e8ddda7de3a13b121922691e06ea3
Infy <infarion@gmail.com>
2020-07-05 15:13:36 +0300

    add view of existing statuses on time line

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
+use App\Models\Status;
 
 class HomeController extends Controller
 {
     public function index(){
 
         if(Auth::check()){
-            return view('timeline.index');
+            $statuses= Status::where(function ($query){
+                return $query->where('user_id',Auth::user()->id)
+                ->orWhereIn('user_id',Auth::user()->pluck('id'));
+            })
+            ->orderBy('created_at','desc')
+            ->paginate(1);
+
+            return view('timeline.index', compact('statuses'));
         }
 
 
